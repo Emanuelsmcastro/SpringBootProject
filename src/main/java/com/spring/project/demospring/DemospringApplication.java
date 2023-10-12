881b786befbf27aad4d3c4e93e335c3e16b9cb44
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-12 20:45:17 -0300

    First commit

---
+++ src/main/java/com/spring/project/demospring/DemospringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.project.demospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/project/demospring/DemospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.project.demospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
