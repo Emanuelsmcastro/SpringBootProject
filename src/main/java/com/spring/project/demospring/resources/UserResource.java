00a29eeb7977da19d22f1b260c986f0f5c4c477f
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-13 14:46:22 -0300

    add services layer

--- src/main/java/com/spring/project/demospring/resources/UserResource.java
+++ src/main/java/com/spring/project/demospring/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.spring.project.demospring.resources;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.project.demospring.entities.User;
+import com.spring.project.demospring.services.UserService;
+
+import io.micrometer.core.ipc.http.HttpSender.Response;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Emanuel", "emanuelsmcastro3@gmail.com", "312312312", "21312");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
