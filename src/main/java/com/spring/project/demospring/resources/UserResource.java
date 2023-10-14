a433896eb6c91183d4423ce63579b03575c93b44
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-14 16:56:21 -0300

    add insert method from to user

--- src/main/java/com/spring/project/demospring/resources/UserResource.java
+++ src/main/java/com/spring/project/demospring/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.spring.project.demospring.resources;
 
 import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.spring.project.demospring.entities.User;
 import com.spring.project.demospring.services.UserService;
@@ -29,5 +33,12 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping(value = "/add-user")
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
